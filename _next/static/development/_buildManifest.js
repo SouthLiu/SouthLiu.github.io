77ffeac2cf5880827035afa55c910311853ff42a
South <1275093225@qq.com>
2021-02-10 19:24:35 +0800

    spider

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/website/home":["static\u002Fchunks\u002Fpages\u002Fwebsite\u002Fhome.js"],"/website/product":["static\u002Fchunks\u002Fpages\u002Fwebsite\u002Fproduct.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
